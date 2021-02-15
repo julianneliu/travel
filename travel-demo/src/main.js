5697417bc0423d0037954fb9097c820bb24f446a
julianne <2531649401@qq.com>
2021-02-15 17:16:33 +0800

    add

--- travel-demo/src/main.js
+++ travel-demo/src/main.js
@@ -8,10 +8,14 @@ import "./assets/styles/border.css";
 //click延迟3秒
 import fastclick from "fastclick";
 import "./assets/styles/iconfont.css";
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
+// import "default-passive-events";
 
 Vue.config.productionTip = false;
 fastclick.attach(document.body);
 Vue.use(Router);
+Vue.use(VueAwesomeSwiper /* { default options with global component } */);
 
 new Vue({
   router,
--- travel-demo/src/pages/home/Home.vue
+++ travel-demo/src/pages/home/Home.vue
@@ -2,15 +2,18 @@
   <div>
     <div>
       <home-header></home-header>
+      <home-swiper></home-swiper>
     </div>
   </div>
 </template>
 <script>
 import HomeHeader from "./components/Header";
+import HomeSwiper from "./components/Swiper";
 export default {
   name: "Home",
   components: {
     HomeHeader,
+    HomeSwiper,
   },
 };
 </script>
