a40093c1fef065bd380aa78f5e48eb46bc29740d
julianne <2531649401@qq.com>
2021-02-15 15:06:06 +0800

    vue

--- travel-demo/src/pages/home/components/Header.vue
+++ travel-demo/src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">内容</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont iconarrow-left-bold back-icon"></div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont iconsearch"></span>
+      输入城市景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont iconjiantouarrow486"></span>
+    </div>
   </div>
 </template>
 <script>
@@ -11,20 +19,36 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl'
 .header {
   display: flex;
-  height: 0.86rem;
+  line-height: 0.86rem;
+  background: $bgColor;
+  color: #fff;
 }
 .header-left {
   width: 0.64rem;
   float: left;
+  .back-icon{
+      text-align:center
+      font-size:.4rem;
+  }
 }
 .header-input {
+    padding-left:0.2rem
   flex: 1;
+  height: 0.62rem;
+  line-height: 0.62rem;
+  margin-top: 0.12rem;
+  margin-left: 0.2rem;
+  background: #fff;
+  border-radius: 0.1rem;
+  color: #ccc;
 }
 .header-right {
   width: 1.24rem;
   float: right;
+  text-align: center;
 }
 </style>
